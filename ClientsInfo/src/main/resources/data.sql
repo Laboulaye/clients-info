17bd1385821814bb3b5f57776564e2e52af3ce4d
Laboulaye <maks.demianov@gmail.com>
2023-09-07 16:41:52 +0300

    Add sql script

---
+++ ClientsInfo/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into clients(id, name) values(1, 'Tony');
+insert into clients values(2, 'Carmella');
+insert into client_emails values(1, 'tony1@gmail.com');
+insert into client_emails values(1, 'tony1New@gmail.com');
+insert into client_emails values(2, 'carmella1@gmail.com');
+insert into client_emails values(2, 'carmella1New@gmail.com');
+insert into client_phones values(1, '+699000555');
+insert into client_phones values(1, '+699000554');
+insert into client_phones values(2, '+699000333');
